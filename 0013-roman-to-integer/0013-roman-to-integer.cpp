4ea23df679fd8ac8b7c7d77cba9b04383cda30dd
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-07-16 16:31:25 +0530

    Time: 14 ms (34.16%), Space: 10.9 MB (75.07%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+    int romanToInt(string s) 
+    {
+      int final = 0;
+        
+      for(int i=0;i<s.length();i++)
+      {
+          std::cout<<s[i];
+          switch(s[i])
+          {
+              case 'I': final += 1;
+                        break;
+              case 'V': 
+                        if(i>0 && s[i-1] == 'I')
+                            final += 3;
+                        else
+                            final += 5;
+                        break;
+                  
+              case 'X': if(i>0 && s[i-1] == 'I')
+                            final += 8;
+                        else
+                            final += 10;
+                        break;
+                  
+              case 'L': if(i>0 && s[i-1] == 'X')
+                            final += 30;
+                        else
+                            final += 50;
+                        break;
+                  
+              case 'C': if(i>0 && s[i-1] == 'X')
+                            final += 80;
+                        else
+                            final += 100;
+                        break;
+                  
+              case 'D': if(i>0 && s[i-1] == 'C')
+                            final += 300;
+                        else
+                            final += 500;
+                        break;
+                  
+              case 'M': if(i>0 && s[i-1] == 'C')
+                            final += 800;
+                        else
+                            final += 1000;
+                        break;
+          }
+          
+      }
+        return final;
+    }
+    
+};
