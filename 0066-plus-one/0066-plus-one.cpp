d3f89898a4fbfafda665aff710b8ed83bcb8986e
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-07-17 18:26:47 +0530

    Time: 2 ms (44.20%), Space: 10.6 MB (6.50%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) 
+    {
+      bool carry = false;
+        
+      if(digits[digits.size()-1] !=9)
+      {
+          digits[digits.size()-1]+=1;
+          return digits;
+      }
+        
+      carry = true;  
+      for(int i = digits.size() - 1; i>=0 ; i--)
+      {
+        if(digits[i] >= 9 && carry)
+        {
+
+            digits[i] = 0;
+        } 
+          
+        else if(carry)
+        {
+            digits[i] += 1;
+            carry = false;
+        }
+      }
+        
+        if(carry)
+        {
+            digits.push_back(1);
+            reverse(digits.begin(),digits.end());
+        }
+        return digits;
+    }
+    
+};
