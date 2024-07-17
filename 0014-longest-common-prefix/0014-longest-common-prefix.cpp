db2efdbf1d5918f76640efa79782c69201d96538
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-07-17 16:00:41 +0530

    Time: 3 ms (70.43%), Space: 11.3 MB (20.42%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -5,7 +5,6 @@ public:
         string result = "";
         sort(strs.begin(),strs.end());
         string first = strs[0], last = strs[strs.size()-1];
-        
         for(int i=0;i<min(first.size(),last.size());i++)
         {
             if(first[i]!=last[i])
