e2a637132c0809a5a3bbcfcaed78a4e220153e99
Anish Rangarajan <96893649+aniranaway@users.noreply.github.com>
2024-07-21 11:40:29 +0530

    Time: 2 ms (48.12%), Space: 10.2 MB (5.28%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -3,14 +3,13 @@ public:
     int largestAltitude(vector<int>& gain) 
     {
         
-        vector<int> path(gain.size() + 1,0);
-        path.push_back(0);
-        
-        for(int i{1}; i< path.size() - 1;i++)
+        for(int i{1}; i< gain.size();i++)
         {
             
-            path[i] = (path[i-1] + gain[i-1]);
+            gain[i] += gain[i-1];
         }
-        return *max_element(path.begin(),path.end());
+        gain.push_back(0);
+        
+        return *max_element(gain.begin(),gain.end());
     }
 };
